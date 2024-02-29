2fd3e999e403dba2d8465c552970b8accaa0a797
unknown <rahul077@academiacollege.edu.np>
2024-02-29 21:36:40 +0545

    second commit

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 import pandas as pd
 import numpy as np
+
+
 def contain(ev):
     if type(ev) != str:
         return False
     ev = ev.lower()
     if 'electric' in ev:
-        for keywords in ['car','bus','cycle','motorbike']:
+        for keywords in ['car', 'bus', 'cycle', 'motorbike', 'induction']:
             if keywords in ev:
                 return True
     return False
 
-#dataimport from excel
-filenames = ['shrawan.xlsx','FTS_upto_Bhadra2080_81.xlsx','FTS_UptoAsoj_2080_81.xlsx','FTS_Uptokartik_2080_81.xlsx','FTS_UptoMangsir_2080_81.xlsx','FTS.xlsx','FTS_Upto-Magh_2080_81-1 (1).xlsx'] 
-months = ['shrawan','bhadra','ashwin','kartik','mangsir','poush','magh','falgun','chaitra','baishak','jestha']
-dfs= []
-for i,filename in enumerate(filenames):
-    df = pd.read_excel(filename,sheet_name="5_Imports_By_Commodity",header=2)
-    df =df[df["Description"].apply(contain)]
-    quantities = df['Quantity']
-    df['Month'] =  months[i]
+
+# dataimport from excel
+filenames = ['shrawan.xlsx', 'FTS_upto_Bhadra2080_81.xlsx', 'FTS_UptoAsoj_2080_81.xlsx',
+             'FTS_Uptokartik_2080_81.xlsx', 'FTS_UptoMangsir_2080_81.xlsx', 'FTS.xlsx', 'FTS_Upto-Magh_2080_81-1 (1).xlsx']
+months = ['shrawan', 'bhadra', 'ashwin', 'kartik', 'mangsir',
+          'poush', 'magh', 'falgun', 'chaitra', 'baishak', 'jestha']
+dfs = []
+# prev_quantity = 0
+prev_df = None
+for i, filename in enumerate(filenames):
+    df = pd.read_excel(filename, sheet_name="5_Imports_By_Commodity", header=2)
+
+    # Rename and drop columns
+    df.rename(columns={'HSCode': 'Sn No'}, inplace=True)
+    df.drop(columns=['Sn No'], inplace=True)
+
+    df = df[df["Description"].apply(contain)]
+
+
+    df['Month'] = months[i]
+
     dfs.append(df)
+    prev_df = df.copy()
 merge_df = pd.concat(dfs)
+merge_df.reset_index(drop=True, inplace=True)
+merge_df.to_csv(r'C:\Users\acer\biddhut\rahul\data.csv', index=False)
